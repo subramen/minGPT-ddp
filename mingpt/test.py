d334052ed74238db2f8f14d42c273a722de4b92b
Suraj Subramanian <5676233+suraj813@users.noreply.github.com>
2022-08-09 23:24:44 -0400

    Delete test.py

--- mingpt/test.py
+++
@@ -1,9 +0,0 @@
-import hydra
-from omegaconf import DictConfig
-
-@hydra.main(config_path=".", config_name="gpt2_train_cfg")
-def main(cfg: DictConfig):
-    print(cfg)
-
-if __name__ == "__main__":
-    main()
